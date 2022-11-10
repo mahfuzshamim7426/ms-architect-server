72ee32aac4fe6e4fc60f947e4840344b96dd4792
mahfuzshamim7426 <mahfuzshamim7426@gmail.com>
2022-11-10 08:48:59 +0600

    feat: services routes added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const cors = require('cors');
@@ -8,7 +9,6 @@ const port = 5000
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3y3ilq1.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true, useUnifiedTopology: true,
@@ -16,13 +16,28 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
-        const userCollection = client.db('msArch').collection('users');
+        const serviceCollection = client.db('msArch').collection('services');
 
-        app.get('/users', async (req, res) => {
-            const cursor = await userCollection.find({})
-            const users = await cursor.toArray();
-            res.send(users);
+        // ===================services routes===========================
+        app.get('/services', async (req, res) => {
+            const cursor = await serviceCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services);
         });
+
+        app.post('/services', async (req, res) => {
+            const serviceData = req.body;
+            const service = await serviceCollection.insertOne(serviceData)
+            res.send(service);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        })
+
 
     } finally {
 
